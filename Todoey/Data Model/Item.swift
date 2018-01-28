805329084475e559caa396e04dbc590c8e6c4df1
John Lin <hiuwailin2@ymail.com>
2018-01-28 23:58:56 +0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by HIU WAI LIN on 28/1/2018.
+//  Copyright © 2018年 HIU WAI LIN. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
